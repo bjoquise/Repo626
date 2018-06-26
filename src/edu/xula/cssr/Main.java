cea7697ae1130be0b72a771abbedb90983c5e6bf
bjoquise <bjoquise@gmail.com>
2018-06-26 10:08:29 -0500

    Ray add

---
+++ src/edu/xula/cssr/Main.java
@@ -0,0 +1,33 @@
+package edu.xula.cssr;
+
+import java.util.ArrayList;
+
+// This program attempts to predict the winner of the 2018 World Cup
+// TODO: 6/15/2018 add team names into an ArrayList
+// TODO: 6/15/2018 pick random country from list
+// TODO: 6/15/2018  get the top five countries after 1000 runs
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Insert world cup teams
+        ArrayList<String> teamsList = new ArrayList<>();
+
+        // Add countries to ArrayList
+        teamsList.add("Argentina"); teamsList.add("Australia"); teamsList.add("Belgium");
+        teamsList.add("Brazil"); teamsList.add("Colombia"); teamsList.add("Costa Rica");
+        teamsList.add("Croatia"); teamsList.add("Denmark"); teamsList.add("Egypt");
+        teamsList.add("England"); teamsList.add("France"); teamsList.add("Germany");
+        teamsList.add("Iceland"); teamsList.add("Iran"); teamsList.add("Japan");
+        teamsList.add("Korean Republic"); teamsList.add("Mexico"); teamsList.add("Morocco");
+        teamsList.add("Nigeria"); teamsList.add("Panama"); teamsList.add("Peru");
+        teamsList.add("Portland"); teamsList.add("Portugal"); teamsList.add("Russia");
+        teamsList.add("Saudi Arabia"); teamsList.add("Senegal"); teamsList.add("Serbia");
+        teamsList.add("Spain"); teamsList.add("Sweden"); teamsList.add("Switzerland");
+        teamsList.add("Tunisia"); teamsList.add("Uruguay");
+        System.out.println("The Teams: " + teamsList);
+
+
+
+    }
+}
